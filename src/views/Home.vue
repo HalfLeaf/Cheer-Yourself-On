0b2475f10f8211565ab00f79337f6d09135c1ee6
半片叶 <34050019+HalfLeaf@users.noreply.github.com>
2020-08-16 17:42:18 +0800

    更新代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,86 +1,154 @@
 <template>
-  <vueper-slides class="slide-div" :autoplay="true" :3d="true" :duration="9000">
-    <vueper-slide v-for="(slide, i) in slides" :key="i" :title="slide.title" :style="{background: defaultImg}">
-      <template v-slot:content>
-        <mymarquee :lists="slide.content" class="slide-marquee"></mymarquee>
-      </template>
-    </vueper-slide >
-  </vueper-slides>
+    <v-carousel height="100%" class="carousel-cheer-on" delimiter-icon="mdi-minus"
+      :cycle="true"
+      :show-arrows="false"
+      :interval="16000"
+      @change="carouselChange"
+    >
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      >
+      <v-img :src="item.src" height="98%" width="99%">
+        <template v-slot:content>
+          <vue-typed-js :strings="item | cookTyped"	:loopCount="32" :attr="'placeholder'">
+            <h1 class="typing"></h1>
+          </vue-typed-js>
+        </template>
+      </v-img>
+      </v-carousel-item>
+  </v-carousel>
 </template>
 
-
 <script>
-import DATA from "./loveTalk.js"
-import mymarquee from './my-marquee';
-import { createClient } from 'pexels';
-import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
+import VueTypedJs from 'vue-typed-js';
 const jinrishici = require('jinrishici');
-const pexelsClient = createClient('563492ad6f91700001000001a4c9a33ce81b4164b6c2a3a5a92383bb');
 
 export default {
-  name: 'Home',
+  name:"cheer-on",
   components: {
-    VueperSlides,
-    VueperSlide,
-    mymarquee
+    VueTypedJs
   },
-  data(){
-    return{
-      DATA,
+  data() {
+    return {
+      defaultImg:"https://pixabay.com/zh/images/download/nepal-2184940_1920.jpg",
       imgList:[],
-      currentImg:"",
-      defaultImg:"https://pixabay.com/get/54e5dd474850ae14f1dc84609629307d1d38daec524c704c7c2972dc944cc450_640.jpg",
-      hitokotoContent:"",
-      hitokotoFrom:"",
-      slides: [],
+      total:1,
+      items: [],
+      jinrishiciData:{},
+    }
+  },
+  filters: {
+    cookTyped(item){
+      return [item.title, item.author, ...item.content]
     }
   },
   methods: {
-    getJsonFile(){
+    getData(){
       let _this = this;
       jinrishici.load(res => {
-        let data = res.data;
-        let tags = data.matchTags;
-        let query = tags.length>1?tags[1]:tags.join("+");
-        this.$axios.get("https://pixabay.com/api/?page=1&per_page=10&key=17387491-6065a6ba72bb2744536566c1d&lang=zh&q="+query).then(resp => {
-          this.imgList = resp.data.hits;
-          this.currentImg = this.imgList.length >0?this.imgList[0].webformatURL:this.defaultImg;
-          this.slides.push({title:data.origin.title, content:data.origin.content, image:this.currentImg})
-        });
+        this.jinrishiciData = res.data;
       });
-      this.$axios.get('https://v1.hitokoto.cn/?encode=json&c=f&c=j&c=h&c=d&c=l').then(response => {
-        this.hitokotoContent = response.data.hitokoto;
-        this.hitokotoFrom = response.data.from;
-        let content = this.hitokotoContent.split(",");
-        content.push(" ");
-        content.push(" ");
-        content.push(" ");
-        this.slides.push({title:this.hitokotoFrom, content:content, image:this.currentImg})
+    },
+    async getImageData(query){
+      let origin = this.jinrishiciData.origin;
+      await this.$axios.get("https://pixabay.com/api/?page=1&per_page=20&key=17387491-6065a6ba72bb2744536566c1d&lang=zh&q="+query).then(resp => {
+        this.imgList = resp.data.hits;
+        this.total = this.imgList.length > 20 ? 20:this.imgList.length;
+        let i = parseInt(Math.random()*this.total);
+        let image = "";
+        try {
+          image = this.imgList[i].webformatURL
+        } catch (error) {
+          image = this.defaultImg;
+        }
+        this.items.push({
+          content:origin.content,
+          author:origin.author,
+          title:origin.title,
+          src:image
+        })
+        this.getLoveTalk();
+        this.getHitokotoData();
+      });
+    },
+    async getLoveTalk(){
+      let _this = this;
+      let loveTalk = await require("./loveTalk.js").default;
+      let index = parseInt(Math.random()*1291);
+      let k = parseInt(Math.random()*_this.total);
+      let img = "";
+      try {
+        img = _this.imgList[k].webformatURL;
+      } catch (error) {
+        img = _this.defaultImg;
+      }
+      _this.items.push({
+        content:loveTalk[index],
+        author:"",
+        title:"我想对你说~",
+        src:img
       })
-      let index = parseInt(Math.random()*1291); 
-      this.slides.push({title:"我想对你说~", content:this.DATA[""+index], image:this.currentImg})
-
+    },
+    async getHitokotoData(){
+      let _this = this;
+      await this.$axios.get('https://v1.hitokoto.cn/?encode=json&c=f&c=j&c=h&c=d&c=l').then(response => {
+        let hitokotoContent = response.data.hitokoto;
+        let hitokotoFrom = response.data.from;
+        let content = [hitokotoContent];
+        let j = parseInt(Math.random()*_this.total);
+        let img = "";
+        try {
+          img = _this.imgList[j].webformatURL
+        } catch (error) {
+          img = _this.defaultImg;
+        }
+        _this.items.push({
+          content:content,
+          author:hitokotoFrom,
+          title:"",
+          src:img
+        })
+      })
+    },
+    carouselChange(value){
+      let item = this.items[value];
+      // var typed = new Typed('#word', {
+      //   strings: [item.title, item.author, ...item.content],
+      //   typeSpeed: 30
+      // });
     }
   },
-  mounted(){
-    this.getJsonFile();
+  mounted() {
+    this.getData();
+  },
+  watch:{
+    jinrishiciData(val){
+      if(!val) return;
+      let tags = val.matchTags;
+      let query = tags.length>1?tags[1]:tags.join("+");
+      this.getImageData(query);
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.slide-div
-  height 100%
-  weight 100%
-  left 0
+.carousel-cheer-on
+  left  0
   right 0
-  bottom 0
   top 0
+  bottom 0
+  height 100%
+  width  100%
   position absolute
-
-.slide-marquee
-  left 5%
-  top 32%
+.shici-word
+  color #64FFDA
+  font-size 16px
+  font-weight 700
+  top 36%
   position relative
+  
 </style>
